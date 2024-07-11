03e288cb1d9127472cb70a17bbfaf5ee54162b44
Sami Hosni <samy.hosni@gmail.com>
2024-07-11 16:20:16 +0100

    RealTime Chat DONE !

---
+++ src/main/java/com/example/complexeSportif/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.complexeSportif.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+}
